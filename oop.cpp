c17d31d723836b3b6550175c453eeb745e99b635
paw2kk <wndxakk@gmail.com>
2025-04-29 10:48:13 +0700

    mengganti error di public dengan titik 2,menambah program main,menambah void print data

--- oop.cpp
+++ oop.cpp
@@ -2,12 +2,25 @@
 using namespace std;
 
 class mahasiswa{
-public;
-int nin;
+public:
+int nim;
 string nama;
 float nilai;
-}
+
+    void print_data(){
+        cout << "NIM  = " << nim << endl;
+        cout << "Nama  = " << nama << endl;
+        cout << "NIlai  = " << nilai << endl;
+    }
+
+};
+
 
 int main(){
-    
+    mahasiswa mhs;
+    mhs.nim = 2024;
+    mhs.nama = "Aufa";
+    mhs.nilai = 100;
+
+    mhs.print_data();
 }
